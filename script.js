8e5558f527750fc570e6429dab6427098d3c0575
Sergey <gnatenko@gmail.com>
2020-07-07 20:56:33 +0300

    lesson 14 hard

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ DomElement.prototype.showElement = function() {
 			background-color: ${this.bg};      						
 			font-size: ${this.fontSize};
 			border: 1px solid #000;
+			position: absolute;
 			  `;
 			  
 		block.innerHTML = this.text;
@@ -55,7 +56,31 @@ DomElement.prototype.showElement = function() {
 
 DomElement.prototype.eventListeners = function() {
 	btn.addEventListener('click', this.showElement.bind(this));
+	document.addEventListener('keydown', (event) => {
+		console.log(event.keyCode);
+		let block = createdElementsSectiontn.querySelector('div');
+		if(block) {
+			
+			if (event.keyCode === 40) {
+				let bottomBock = getComputedStyle(block).bottom
+				block.style.bottom = parseInt(bottomBock, 10) - 10 + 'px'; 
+			} else if (event.keyCode === 38) {
+				let topBock = getComputedStyle(block).bottom
+				block.style.bottom = parseInt(topBock, 10) + 10 + 'px'; 
+			} else if (event.keyCode === 39) {
+				let rightBock = getComputedStyle(block).left
+				block.style.left = parseInt(rightBock, 10) + 10 + 'px'; 
+			}else if (event.keyCode === 37) {
+				let lefttBock = getComputedStyle(block).left
+				block.style.left = parseInt(lefttBock, 10) - 10 + 'px'; 
+			}
+
+		} else {
+			return;
+		}
+	});
 };
 
-let magic = new DomElement('200px', '400px', 'tomato', '30px', 'добрый вечер, блин!' );
+let magic = new DomElement('100px', '100px', 'tomato', '30px', 'добрый вечер, блин!' );
 magic.eventListeners();
+
